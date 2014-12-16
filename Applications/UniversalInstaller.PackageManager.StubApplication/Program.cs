f19fc6dd44185a7c245053daa45f05c53089d9ac
Michael Becker <alcexhim@gmail.com>
2014-12-15 19:35:55 -0800

    Initial commit

---
+++ Applications/UniversalInstaller.PackageManager.StubApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversalInstaller.PackageManager.StubApplication
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+		}
+	}
+}
---
+++ Engines/WindowsForms/Engines/UniversalInstaller.Engines.WindowsForms/WindowsFormsEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UniversalInstaller.Core;
+
+namespace UniversalInstaller.Engines.WindowsForms
+{
+	public class WindowsFormsEngine : Engine
+	{
+	}
+}
